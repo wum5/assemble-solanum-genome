2c5cd4ae08a0993bd82f4c21045edb8d33814119
Meng Wu <wum5@umail.iu.edu>
2018-05-07 16:01:17 -0400

    change the space indent in python scripts; fix a bug in annotation_by_aed.py

--- annotation_by_aed.py
+++ annotation_by_aed.py
@@ -14,302 +14,303 @@ from Bio import SeqIO
 
 
 def extract_value(string):
-	"""
-	Extract value after '='
-	"""
-	value = string.split('=')[1]
-	return value
+    """
+    Extract value after '='
+    """
+    value = string.split('=')[1]
+    return value
 
 
 
 def sort_geneid(old_ids, gene_pos, scaff_id):
-	"""
-	Sort the gene ids by on on their coordinates
-	"""
-	renamed_ids = {}
-	gene_order = [i[0] for i in sorted(enumerate(gene_pos), key=lambda x:x[1])]
+    """
+    Sort the gene ids by on on their coordinates
+    """
+    renamed_ids = {}
+    gene_order = [i[0] for i in sorted(enumerate(gene_pos), key=lambda x:x[1])]
 	
-	for x in xrange(len(gene_order)):
-		if x < 10:
-			new_ids = scaff_id + 'g000' + str(x*10+10)
-		elif 10 <= x < 100:
-			new_ids = scaff_id + 'g00' + str(x*10+10)
-		elif 100 <= x < 1000:
-			new_ids = scaff_id + 'g0' + str(x*10+10)
-		elif 1000 <= x < 10000:
-			new_ids = scaff_id + 'g' + str(x*10+10)
+    for x in xrange(len(gene_order)):
+        if x < 10:
+            new_ids = scaff_id + 'g000' + str(x*10+10)
+        elif 10 <= x < 100:
+            new_ids = scaff_id + 'g00' + str(x*10+10)
+        elif 100 <= x < 1000:
+            new_ids = scaff_id + 'g0' + str(x*10+10)
+        elif 1000 <= x < 10000:
+            new_ids = scaff_id + 'g' + str(x*10+10)
 
-		renamed_ids[old_ids[x]] = new_ids
+        renamed_ids[old_ids[x]] = new_ids
 	
-	return renamed_ids
+    return renamed_ids
 
 
 
 def rename_geneid(raw_gff3, updated_gff3):
-	"""
-	Rename the genes based on their coordinates on scaffolds, and output generated is 
-	the mapping between old ids and new ids
-	"""
-	infile = open(raw_gff3, "r")
-	outfile = open(updated_gff3, "w")
+    """
+    Rename the genes based on their coordinates on scaffolds, and output generated is 
+    the mapping between old ids and new ids
+    """
+    infile = open(raw_gff3, "r")
+    outfile = open(updated_gff3, "w")
 	
-	geneids = {}
-	prev_scaf = ''
-	prev_record = ''
-	gene_pos = []
-	new_ids = []
-	old_ids = []	
+    geneids = {}
+    prev_scaf = ''
+    prev_record = ''
+    gene_pos = []
+    new_ids = []
+    old_ids = []	
 		
-	for line in infile:
-		line = line.rstrip()
-		if '#' in line: continue
+    for line in infile:
+        line = line.rstrip()
+        if '#' in line: continue
 				
-		if line.split('\t')[2] == 'gene':
-			curr_scaf = line.split('\t')[0]
-			gene_start = line.split('\t')[3]
-			geneid = ((line.split('\t')[8]).split(';')[0]).split('ID=')[1]
-			#print curr_scaf, prev_scaf
+        if line.split('\t')[2] == 'gene':
+            curr_scaf = line.split('\t')[0]
+            gene_start = line.split('\t')[3]
+            geneid = ((line.split('\t')[8]).split(';')[0]).split('ID=')[1]
+            #print curr_scaf, prev_scaf
 			
-			if curr_scaf == prev_scaf:
-				gene_pos.append(int(gene_start))
-				old_ids.append(geneid)
-				prev_record += line+'\n'
+            if curr_scaf == prev_scaf:
+                gene_pos.append(int(gene_start))
+                old_ids.append(geneid)
+                prev_record += line+'\n'
 
-			else:
-				geneids = sort_geneid(old_ids, gene_pos, prev_scaf)
-				for key in geneids:
-					prev_record = prev_record.replace(key, geneids[key])
-				prev_record = prev_record.replace('-mRNA-', '.')
-				outfile.write(prev_record)
+            else:
+                geneids = sort_geneid(old_ids, gene_pos, prev_scaf)
+                for key in geneids:
+                    prev_record = prev_record.replace(key, geneids[key])
+                prev_record = prev_record.replace('-mRNA-', '.')
+                outfile.write(prev_record)
 				
-				prev_record = ''
-				prev_scaf = curr_scaf
+                prev_record = ''
+                prev_scaf = curr_scaf
 				
-				gene_pos = []
-				old_ids = []
-				new_ids = []
-				gene_pos.append(int(gene_start))
-				old_ids.append(geneid)
-				prev_record += line+'\n'
+                gene_pos = []
+                old_ids = []
+                new_ids = []
+                gene_pos.append(int(gene_start))
+                old_ids.append(geneid)
+                prev_record += line+'\n'
 				
-		else:
-			prev_record += line+'\n'
+        else:
+            prev_record += line+'\n'
 
-	geneids = sort_geneid(old_ids, gene_pos, prev_scaf)
-	for key in geneids:
-		prev_record = prev_record.replace(key, geneids[key])
+    geneids = sort_geneid(old_ids, gene_pos, prev_scaf)
+    for key in geneids:
+        prev_record = prev_record.replace(key, geneids[key])
 	
-	prev_record = prev_record.replace('-mRNA-', '.')
-	outfile.write(prev_record)
+    prev_record = prev_record.replace('-mRNA-', '.')
+    outfile.write(prev_record)
 			
-	infile.close()		
-	outfile.close()	
+    infile.close()		
+    outfile.close()	
 
 	
 
 def longest_isoform(infile):
-	"""
-	remove redundant transcripts from a same gene based on the length of sequences, retaining
-	each gene with the number of amino acids (AA) or bases (DNA) from the longest isoform
-	"""
-	length_dict = {}
-	index = infile.split('.')[0]
+    """
+    remove redundant transcripts from a same gene based on the length of sequences, retaining
+    each gene with the number of amino acids (AA) or bases (DNA) from the longest isoform
+    """
+    length_dict = {}
+    index = infile.split('.')[0]
 	
-	for record in SeqIO.parse(infile, "fasta"):
-		if 'mRNA' in record.id:
-			name = record.id.split('-mRNA-')[0]
-		elif '.' in record.id:
-			name = record.id.split('.')[0]
+    for record in SeqIO.parse(infile, "fasta"):
+        if 'mRNA' in record.id:
+            name = record.id.split('-mRNA-')[0]
+        elif '.' in record.id:
+            name = record.id.split('.')[0]
 			
-		if name not in length_dict:
-			length_dict[name]=len(str(record.seq))
-		elif len(str(record.seq)) > length_dict[name]:
-			length_dict[name]=len(str(record.seq))			
+        if name not in length_dict:
+            length_dict[name]=len(str(record.seq))
+        elif len(str(record.seq)) > length_dict[name]:
+            length_dict[name]=len(str(record.seq))			
 	
-	outfile = open(index+"_NR.fasta", "w")
+    outfile = open(index+"_NR.fasta", "w")
 	
-	for record in SeqIO.parse(infile, "fasta"):
-		if 'mRNA' in record.id:
-			name = record.id.split('-mRNA-')[0]
-		elif '.' in record.id:
-			name = record.id.split('.')[0]
+    for record in SeqIO.parse(infile, "fasta"):
+        if 'mRNA' in record.id:
+            name = record.id.split('-mRNA-')[0]
+        elif '.' in record.id:
+            name = record.id.split('.')[0]
 			
-		size = len(str(record.seq))
-		if length_dict[name] == size:
-			outfile.write(">"+record.id+'\n')
-			outfile.write(str(record.seq)+'\n')
-			length_dict[name] = -1
+        size = len(str(record.seq))
+        if length_dict[name] == size:
+            outfile.write(">"+record.id+'\n')
+            outfile.write(str(record.seq)+'\n')
+            length_dict[name] = -1
 			
-	outfile.close()
+    outfile.close()
 	
 	
 	
 def gff3fasta(genome_seq, gff3file):
-	"""
-	Extract CDS/protein sequences based on GFF3 file
-	"""
-	index = (os.path.basename(gff3file)).split('.')[0]
-	cmd="gffread -x %s_cds.fasta -g %s %s" % (index, genome_seq, gff3file)
-	os.system(cmd)
-	cmd="gffread -y %s_proteins.fasta -g %s %s" % (index, genome_seq, gff3file)
-	os.system(cmd)
+    """
+    Extract CDS/protein sequences based on GFF3 file
+    """
+    index = (os.path.basename(gff3file)).split('.')[0]
+    cmd="gffread -x %s_cds.fasta -g %s %s" % (index, genome_seq, gff3file)
+    os.system(cmd)
+    cmd="gffread -y %s_proteins.fasta -g %s %s" % (index, genome_seq, gff3file)
+    os.system(cmd)
 	
-	longest_isoform(str(index)+'_cds.fasta')
-	longest_isoform(str(index)+'_proteins.fasta')
+    longest_isoform(str(index)+'_cds.fasta')
+    longest_isoform(str(index)+'_proteins.fasta')
 	
 	
 
 def parse_maker_gff3(gff_file, AED_cutoff):
-	"""
-	Extract high-confidence gene models from MAKER2 output using the AED cutoffs and 
-	generate two output files: the updated GFF3 file and the non-redundant protein fasta
-	"""		
-	index = gff_file.split('.')[0]
-	infile = open(gff_file, "r")
+    """
+    Extract high-confidence gene models from MAKER2 output using the AED cutoffs and 
+    generate two output files: the updated GFF3 file and the non-redundant protein fasta
+    """		
+    index = gff_file.split('.')[0]
+    infile = open(gff_file, "r")
 	
-	# initialization
-	output = []
-	outfile = open("tempfile", "w")
-	gene_num, mRNA_num = 0, 0
-	AED_thres={0.0:0, 0.1:0, 0.2:0, 0.3:0, 0.4:0, 0.5:0, 0.6:0, 0.7:0, 0.8:0, 0.9:0, 1.0:0}
+    # initialization
+    output = []
+    outfile = open("tempfile", "w")
+    gene_num, mRNA_num = 0, 0
+    AED_thres={0.0:0, 0.1:0, 0.2:0, 0.3:0, 0.4:0, 0.5:0, 0.6:0, 0.7:0, 0.8:0, 0.9:0, 1.0:0}
 	
-	for line in infile:
-		if 'maker' not in line: continue
-		line = line.rstrip()
-		feature = line.split('\t')[2]
-		annotation = line.split('\t')[8]
+    for line in infile:
+        if 'maker' not in line: continue
+        line = line.rstrip()
+        feature = line.split('\t')[2]
+        annotation = line.split('\t')[8]
 		
-		if feature == 'gene':
-			# print the previous qualified gene model
-			if len(output) > 1:
-				gene_num += 1
-				for element in output:
-					outfile.write(element+'\n')
+        if feature == 'gene':
+            # print the previous qualified gene model
+            if len(output) > 1:
+                gene_num += 1
+                for element in output:
+                    outfile.write(element+'\n')
 					
-			# re-initialization
-			output = []
-			output.append(line)
-			mrna_ids = []
+            # re-initialization
+            output = []
+            output.append(line)
+            mrna_ids = []
 			
-		elif feature == 'mRNA':
-			AED = float(extract_value(annotation.split(';')[3]))
+        elif feature == 'mRNA':
+            AED = float(extract_value(annotation.split(';')[3]))
 			
-			if AED <= AED_cutoff:
-				tmp = extract_value(annotation.split(';')[0])
-				mrna_ids.append(tmp)
-				mRNA_num += 1
-				output.append(line)
+            if AED <= AED_cutoff:
+                tmp = extract_value(annotation.split(';')[0])
+                mrna_ids.append(tmp)
+                mRNA_num += 1
+                output.append(line)
 					
-			for key in AED_thres:
-				if AED < key:
-					AED_thres[key]+=1
+            for key in AED_thres:
+                if AED < key:
+                    AED_thres[key]+=1
 									
 				
-		elif feature in ['exon', 'CDS', 'five_prime_UTR', 'three_prime_UTR']:
-			tmp = extract_value(annotation.split(';')[1])
-			parents = tmp.split(",")
-			for item in parents:
-				if item in mrna_ids:
-					output.append(line)
+        elif feature in ['exon', 'CDS', 'five_prime_UTR', 'three_prime_UTR']:
+            tmp = extract_value(annotation.split(';')[1])
+            parents = tmp.split(",")
+            for item in parents:
+                if item in mrna_ids:
+                    output.append(line)
 									
-		else: 
-			pass
+        else: 
+            pass
 		
 	
-	# print the last qualified gene model
-	if len(output) > 1:
-		gene_num += 1
-		for element in output:
-			outfile.write(element+'\n')
-			outfile.close()
-			infile.close()
+    # print the last qualified gene model
+    if len(output) > 1:
+        gene_num += 1
+        for element in output:
+            outfile.write(element+'\n')
+	
+    outfile.close()
+    infile.close()
 	
 	
-	# print the number genes matching the criteria to the screen		
-	if AED_cutoff < 1:
-		with open("genes_summary.txt", "w") as outfile:
-			outfile.write("AED cutoff: %.2f \nNumber of genes: %d \nNumber of transcripts: %d\n" 
-			% (AED_cutoff, gene_num, mRNA_num))
+    # print the number genes matching the criteria to the screen		
+    if AED_cutoff < 1:
+        with open("genes_summary.txt", "w") as outfile:
+            outfile.write("AED cutoff: %.2f \nNumber of genes: %d \nNumber of transcripts: %d\n" 
+            % (AED_cutoff, gene_num, mRNA_num))
 
-	plt.plot(AED_thres.keys(), AED_thres.values(), 'ro', label="AED")
-	plt.xticks(AED_thres.keys())
-	plt.ylabel('Number of Gene Models', fontsize=12)
-	plt.xlabel('AED scores', fontsize=12)
-	plt.title("Annotation quality check with MAKER's AED score", fontsize=15)
-	plt.savefig("AED_plot.pdf")
+    plt.plot(AED_thres.keys(), AED_thres.values(), 'ro', label="AED")
+    plt.xticks(AED_thres.keys())
+    plt.ylabel('Number of Gene Models', fontsize=12)
+    plt.xlabel('AED scores', fontsize=12)
+    plt.title("Annotation quality check with MAKER's AED score", fontsize=15)
+    plt.savefig("AED_plot.pdf")
 			
 	
-	# write the updated GFF3 file
-	updated_gff3 = index+'.aed.'+str(AED_cutoff)+'.gff3'	
-	rename_geneid('tempfile', updated_gff3)
-	os.system('rm tempfile')	
+    # write the updated GFF3 file
+    updated_gff3 = index+'.aed.'+str(AED_cutoff)+'.gff3'	
+    rename_geneid('tempfile', updated_gff3)
+    os.system('rm tempfile')	
 
-	return updated_gff3
+    return updated_gff3
 	
 
 
 def gff_stats(gff_file):
-	"""
-	Obtain the statistics of gene annotations from the updated gff file
-	"""
-	exon_num, gene_num, exon_size, cds_size, gene_size = 0, 0, 0, 0, 0
+    """
+    Obtain the statistics of gene annotations from the updated gff file
+    """
+    exon_num, gene_num, exon_size, cds_size, gene_size = 0, 0, 0, 0, 0
 
-	with open(gff_file, "r") as infile:
-		for line in infile:
-			if 'maker' not in line: continue
-			line = line.rstrip()
-			feature = line.split('\t')[2]
-			start = int(line.split('\t')[3])
-			end = int(line.split('\t')[4])
-			size = end - start + 1	
+    with open(gff_file, "r") as infile:
+        for line in infile:
+            if 'maker' not in line: continue
+            line = line.rstrip()
+            feature = line.split('\t')[2]
+            start = int(line.split('\t')[3])
+            end = int(line.split('\t')[4])
+            size = end - start + 1	
 		
-			if feature == 'mRNA':
-				gene_num += 1
-				gene_size += size
+            if feature == 'mRNA':
+                gene_num += 1
+                gene_size += size
 			
-			elif feature == 'exon':
-				exon_num += 1
-				exon_size += size
+            elif feature == 'exon':
+                exon_num += 1
+                exon_size += size
 		
-			elif feature == 'CDS':
-				cds_size += size
+            elif feature == 'CDS':
+                cds_size += size
 		
-	avg_exon_num = float(exon_num)/gene_num
-	avg_exon_size = float(exon_size)/exon_num
-	avg_cds_size = float(cds_size)/gene_num
-	avg_transcript_size = float(exon_size)/gene_num
-	intron_size = float(gene_size-exon_size)/(exon_num-gene_num)
+    avg_exon_num = float(exon_num)/gene_num
+    avg_exon_size = float(exon_size)/exon_num
+    avg_cds_size = float(cds_size)/gene_num
+    avg_transcript_size = float(exon_size)/gene_num
+    intron_size = float(gene_size-exon_size)/(exon_num-gene_num)
 
-	with open("genes_summary.txt", "a") as outfile:
-		outfile.write("Mean number of exons per gene: %f\n" % (avg_exon_num))
-		outfile.write("Mean exon size: %f\n" % (avg_exon_size))
-		outfile.write("Mean transcript length: %f\n" % (avg_transcript_size))
-		outfile.write("Mean CDS size: %f\n" % (avg_cds_size))
-		outfile.write("Mean intron size: %f\n" % (intron_size))
+    with open("genes_summary.txt", "a") as outfile:
+        outfile.write("Mean number of exons per gene: %f\n" % (avg_exon_num))
+        outfile.write("Mean exon size: %f\n" % (avg_exon_size))
+        outfile.write("Mean transcript length: %f\n" % (avg_transcript_size))
+        outfile.write("Mean CDS size: %f\n" % (avg_cds_size))
+        outfile.write("Mean intron size: %f\n" % (intron_size))
 	
 	
 		
 
 def main():
-	parser = argparse.ArgumentParser(prog="annotation_by_AED.py", description=__doc__,
-	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('--genome', help=("input genome sequence in fasta"), required=True)
-	parser.add_argument('--gff', help=("the raw GFF3 file from MAKER output"), required=True)
-	parser.add_argument('--AED', help=("the cutoff AED score used for filtering"), default=1)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(prog="annotation_by_AED.py", description=__doc__,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--genome', help=("input genome sequence in fasta"), required=True)
+    parser.add_argument('--gff', help=("the raw GFF3 file from MAKER output"), required=True)
+    parser.add_argument('--AED', help=("the cutoff AED score used for filtering"), default=1)
+    args = parser.parse_args()
     
-	gff_file = args.gff
-	genome_seq = args.genome
-	AED_cutoff = float(args.AED)
+    gff_file = args.gff
+    genome_seq = args.genome
+    AED_cutoff = float(args.AED)
 
-	updated_gff3 = parse_maker_gff3(gff_file, AED_cutoff)
-	gff3fasta(genome_seq, updated_gff3)
-	gff_stats(updated_gff3)
+    updated_gff3 = parse_maker_gff3(gff_file, AED_cutoff)
+    gff3fasta(genome_seq, updated_gff3)
+    gff_stats(updated_gff3)
 	
 	
 
 if __name__ == "__main__":
-	main()
+    main()
 	
 	
 	
--- genome_stat.py
+++ genome_stat.py
@@ -10,132 +10,131 @@ nucleotides = ['A', 'T', 'C', 'G']
 
 
 class classFASTA:
-	"""
-	Parse fasta-formated sequences into lists of sequences and ids
-	"""
-	def __init__(self, fileFASTA):
-		self.fileFASTA = fileFASTA
+    """
+    Parse fasta-formated sequences into lists of sequences and ids
+    """
+    def __init__(self, fileFASTA):
+        self.fileFASTA = fileFASTA
 
-	def readFASTA(self):
-		"""Checks for fasta by file extension"""
-		file_lower = self.fileFASTA.lower()
-		"""Check for three most common fasta file extensions"""
-		if file_lower.endswith('.txt') or file_lower.endswith('.fa') or \
-		file_lower.endswith('.fasta') or file_lower.endswith('.fna'):
-			with open(self.fileFASTA, "r") as f:
-				return self.ParseFASTA(f)
-		else:
-			print "Not in FASTA format."
+    def readFASTA(self):
+        """Checks for fasta by file extension"""
+        file_lower = self.fileFASTA.lower()
+        """Check for three most common fasta file extensions"""
+        if file_lower.endswith('.txt') or file_lower.endswith('.fa') or \
+        file_lower.endswith('.fasta') or file_lower.endswith('.fna'):
+            with open(self.fileFASTA, "r") as f:
+                return self.ParseFASTA(f)
+        else:
+            print "Not in FASTA format."
 
-	def ParseFASTA(self, fileFASTA):
-		"""Gets the sequence name and sequence from a FASTA formatted file"""
-		fasta_list=[]
-		for line in fileFASTA:
-			if line[0] == '>':
-				try:
-					fasta_list.append(current_dna)
-				#pass if an error comes up
-				except UnboundLocalError:
-					#print "Inproper file format."
-					pass
+    def ParseFASTA(self, fileFASTA):
+        """Gets the sequence name and sequence from a FASTA formatted file"""
+        fasta_list=[]
+        for line in fileFASTA:
+            if line[0] == '>':
+                try:
+                    fasta_list.append(current_dna)
+                #pass if an error comes up
+                except UnboundLocalError:
+                    #print "Inproper file format."
+                    pass
 				current_dna = [line.lstrip('>').rstrip('\n'),'']
-			else:
-				current_dna[1] += "".join(line.split())
-		fasta_list.append(current_dna)
-		"""Returns fasa as nested list, containing line identifier \
-		and sequence"""
-		return fasta_list
+            else:
+                current_dna[1] += "".join(line.split())
+        fasta_list.append(current_dna)
+        #Returns fasa as nested list, containing line identifier and sequence
+        return fasta_list
 
 
 def genome_contents(genome_seq):
-	"""
-	Calculate GC contents
-	"""
-	total = len(genome_seq)
-	G = genome_seq.count('G')
-	C = genome_seq.count('C')
-	GC = str(round(float(G+C)/total*100,2))+'%'
-	return GC
+    """
+    Calculate GC contents
+    """
+    total = len(genome_seq)
+    G = genome_seq.count('G')
+    C = genome_seq.count('C')
+    GC = str(round(float(G+C)/total*100,2))+'%'
+    return GC
 
 
 def N50_estimation(contigs):
-	"""
-	Calculate N50, N90, minimum and maximum contigs, etc
-	"""
-	Contigs_Len = [ len(x) for x in contigs ]
-	Contigs_Len.sort(reverse=True)
-	Total = sum(Contigs_Len)
-	Len = len(Contigs_Len)
-	Max = Contigs_Len[0]
-	Min = Contigs_Len[-1]
-	Avg = sum(Contigs_Len)/Len
-	accumulate = 0
+    """
+    Calculate N50, N90, minimum and maximum contigs, etc
+    """
+    Contigs_Len = [ len(x) for x in contigs ]
+    Contigs_Len.sort(reverse=True)
+    Total = sum(Contigs_Len)
+    Len = len(Contigs_Len)
+    Max = Contigs_Len[0]
+    Min = Contigs_Len[-1]
+    Avg = sum(Contigs_Len)/Len
+    accumulate = 0
 	
-	for x in range(len(Contigs_Len)):
-		accumulate += Contigs_Len[x]
-		if accumulate > Total*0.2:
-			N20 = Contigs_Len[x]
-			stop = x 
-			break
+    for x in range(len(Contigs_Len)):
+        accumulate += Contigs_Len[x]
+        if accumulate > Total*0.2:
+            N20 = Contigs_Len[x]
+            stop = x 
+            break
 
-	for x in range(stop,len(Contigs_Len)):
-		accumulate += Contigs_Len[x]
-		if accumulate > Total*0.5:
-			N50 = Contigs_Len[x]
-			stop = x
-			L50 = x
-			break
+    for x in range(stop,len(Contigs_Len)):
+        accumulate += Contigs_Len[x]
+        if accumulate > Total*0.5:
+            N50 = Contigs_Len[x]
+            stop = x
+            L50 = x
+            break
 			
-	for x in range(stop,len(Contigs_Len)):
-		accumulate += Contigs_Len[x]
-		if accumulate > Total*0.9:
-			N90 = Contigs_Len[x]
-			L90 = x
-			break
+    for x in range(stop,len(Contigs_Len)):
+        accumulate += Contigs_Len[x]
+        if accumulate > Total*0.9:
+            N90 = Contigs_Len[x]
+            L90 = x
+            break
 
-	return Total, Len, N20, N50, L50, N90, L90, Max, Min, Avg
+    return Total, Len, N20, N50, L50, N90, L90, Max, Min, Avg
 	
 
 def filter_contigs(contig_ids, contig_seqs, minLen):
-	"""
-	Filter out contigs smaller than specified length
-	"""
-	outfile = open("large_scaffolds.fa", "w")
-	for x in xrange(len(contig_ids)):
-		if len(contig_seqs[x]) > minLen:		
-			outfile.write('>'+contig_ids[x]+'\n')
-			outfile.write(contig_seqs[x]+'\n')
-	outfile.close()
+    """
+    Filter out contigs smaller than specified length
+    """
+    outfile = open("large_scaffolds.fa", "w")
+    for x in xrange(len(contig_ids)):
+        if len(contig_seqs[x]) > minLen:		
+            outfile.write('>'+contig_ids[x]+'\n')
+            outfile.write(contig_seqs[x]+'\n')
+    outfile.close()
 
 
 def main():
-	parser = argparse.ArgumentParser(prog="genome_stat.py", description=__doc__,
-	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('-i', '--fasta_file', help=("the assembled genome in fasta format"), required=True)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(prog="genome_stat.py", description=__doc__,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--fasta_file', help=("the assembled genome in fasta format"), required=True)
+    args = parser.parse_args()
 
-	infile = classFASTA(args.fasta_file)
+    infile = classFASTA(args.fasta_file)
 	
-	######### This command returns the nested list containing sequence names
-	######### and sequences to a flat list containing only sequences or ids
-	contig_ids = [ x[0] for x in infile.readFASTA() ]
-	contig_seqs = [ x[1] for x in infile.readFASTA() ]
-	contig_seqs = [ x.upper() for x in contig_seqs ]
+    ######### This command returns the nested list containing sequence names
+    ######### and sequences to a flat list containing only sequences or ids
+    contig_ids = [ x[0] for x in infile.readFASTA() ]
+    contig_seqs = [ x[1] for x in infile.readFASTA() ]
+    contig_seqs = [ x.upper() for x in contig_seqs ]
 	
 
-	######### concatenated the sequences
-	genome = ''.join(contig_seqs[:])
+    ######### concatenated the sequences
+    genome = ''.join(contig_seqs[:])
 
-	GC = genome_contents(genome)
-	Total, Num, N20, N50, L50, N90, L90, Max, Min, Avg = N50_estimation(contig_seqs)
+    GC = genome_contents(genome)
+    Total, Num, N20, N50, L50, N90, L90, Max, Min, Avg = N50_estimation(contig_seqs)
 	
-	with open("genome_assembly_stats.txt", "w") as outfile:
-		outfile.write("Total bases: %d, GC content: %s\n" % (Total, GC))
-		outfile.write("#Contigs: %d, N50: %d bps, L50: %d, N90: %d bps, L90: %d\n" % (Num, N50, L50, N90, L90)) 
-		outfile.write("Max: %d bps, Min: %d bps, Mean: %d bps\n" % (Max, Min, Avg))
+    with open("genome_assembly_stats.txt", "w") as outfile:
+        outfile.write("Total bases: %d, GC content: %s\n" % (Total, GC))
+        outfile.write("#Contigs: %d, N50: %d bps, L50: %d, N90: %d bps, L90: %d\n" % (Num, N50, L50, N90, L90)) 
+        outfile.write("Max: %d bps, Min: %d bps, Mean: %d bps\n" % (Max, Min, Avg))
 	
         filter_contigs(contig_ids, contig_seqs, N20)
 
 if __name__ == "__main__":
-	main()
+    main()
 	
